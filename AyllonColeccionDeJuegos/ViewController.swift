52bc6ce5e88783472787f657d6d997e58966cd81
PatrickAyllonRubio <patrick.ayllon@tecsup.edu.pe>
2024-09-30 17:10:40 -0500

    Initial Commit

---
+++ AyllonColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AyllonColeccionDeJuegos
+//
+//  Created by Patrick Hugo Ayllòn Rubio on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
